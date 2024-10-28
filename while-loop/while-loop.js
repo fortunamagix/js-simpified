137fb96ad943a029470c8d88fc6b509b52d528f7
Admin <fortunamagix@gmail.com>
2024-10-29 06:29:35 +0700

    JS sim01 While-loop finish

--- while-loop/while-loop.js
+++ while-loop/while-loop.js
@@ -3,3 +3,22 @@ while (i < 5) {
   console.log(i)
   i++
 }
+
+// example
+const person = {
+  name: 'Witthaya',
+  friend: {
+    name: 'Kyle',
+    friend: {
+      name: 'Sally',
+    },
+  },
+}
+
+// ???
+let currentPerson = person
+while (currentPerson != null) {
+  if (currentPerson.name === 'Kyle') continue
+  console.log(currentPerson.name)
+  currentPerson = currentPerson.friend
+}
