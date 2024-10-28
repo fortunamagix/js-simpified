89f027dfd3f69e09d5c2eadf10c5397ba04fb617
Admin <fortunamagix@gmail.com>
2024-10-28 19:51:16 +0700

    JS sim01 If-else v1

---
+++ if-else01.js
@@ -0,0 +1,24 @@
+let x = 3
+
+if (0) {
+  console.log('hello world')
+} else if (x === 2) {
+  console.log('x=2')
+} else {
+  console.log('hell')
+}
+
+// Create a variable that contains an array
+let myArray = [1, 2, 3, 4, 5, 6]
+
+//using an if statement with else if I want you to check the length of the array
+//If it is empty print out 'empty'
+if (myArray === 0) {
+  console.log('empty')
+} else if (myArray.length < 5) { //If it has less than 5 elements print out 'small'
+  console.log('small')
+} else if (myArray.length < 10) { // if it has less than 10 elements print out 'medium'
+  console.log('medium')
+} else { // Otherwise print out 'large'
+  console.log('large')
+}
