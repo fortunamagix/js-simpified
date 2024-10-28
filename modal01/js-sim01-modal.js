6a4aff3b25ff80095185b4fe86fd3a71fee553f2
Admin <fortunamagix@gmail.com>
2024-10-28 19:22:44 +0700

    JS sim01 Modal - finish v2

--- modal01/js-sim01-modal.js
+++ modal01/js-sim01-modal.js
@@ -19,9 +19,26 @@ BONUS: Also add the class 'open' to the overlay
 openModalButton.addEventListener('click', () => {
   modal.classList.add('open')
   overlay.classList.add('open')
+  //remove those 2 lines above and call function 'closeModal' here
+
 })
 
 closeModalButton.addEventListener('click', () => {
   modal.classList.remove('open')
   overlay.classList.remove('open')
+  //remove those 2 lines above and call function 'closeModal' here
 })
+
+
+//can simplified using this function
+function closeModal() {
+  modal.classList.remove('open')
+  overlay.classList.remove('open')
+}
+
+/*
+most simplified using this code instead
+ */
+
+closeModalButton.addEventListener('click',closeModal)
+overlay.addEventListener('click',closeModal)
