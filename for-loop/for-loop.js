69c898fd6a307b456d8d2d387e53912f8e704fc0
Admin <fortunamagix@gmail.com>
2024-10-29 02:51:11 +0700

    JS sim01 For-loop final

--- for-loop/for-loop.js
+++ for-loop/for-loop.js
@@ -14,3 +14,12 @@ for (let i = 0; i < a.length; i++) {
   const element = a[i]
   console.log(element)
 }
+
+// Create a for loop that loops from 0 to 10 and print out all values from 0 to 10
+console.log('\n=== challenge for loop ===')
+for (let i = 0; i < 10; i++) {
+  if (i === 5) {
+    break
+  }
+  console.log(i + 1)
+}
