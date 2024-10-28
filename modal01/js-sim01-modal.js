1a2fd41f811fd1629bd015b8610826f7762f2df3
Admin <fortunamagix@gmail.com>
2024-10-28 11:06:57 +0700

    JS sim01 Modal - adding jsv1

---
+++ modal01/js-sim01-modal.js
@@ -0,0 +1,21 @@
+//select all elements by ID
+/*
+- modal
+- open-modal-btn
+- close-modal-btn
+- overlay
+*/
+
+const modal = document.querySelector('#modal')
+const openModalButton = document.querySelector('#open-modal-btn')
+const closeModalButton = document.querySelector('#close-modal-btn')
+const overlay = document.querySelector('#overlay')
+
+/*
+TODO: Create a click event listener for the open-modal-btn
+BONUS: Also add the class 'open' to the overlay
+ */
+//Bonus
+openModalButton.addEventListener('click', () => {
+  modal.classList.add('open')
+})
