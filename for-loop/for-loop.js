82577aa4af314ae619abc15363dc8421423327f9
Admin <fortunamagix@gmail.com>
2024-10-29 02:44:20 +0700

    JS sim01 For-loop v2

--- for-loop/for-loop.js
+++ for-loop/for-loop.js
@@ -3,5 +3,14 @@
 // how to modify our variable
 
 for (let i = 0; i < 5; i++) { //i++ = i=i+1
+  if (i > 2) break
   console.log('Hi')
+}
+
+// use instead of forEach function
+console.log('\n=== use for loop instead of forEach function ===')
+const a = ['a', 'b', 'c']
+for (let i = 0; i < a.length; i++) {
+  const element = a[i]
+  console.log(element)
 }
